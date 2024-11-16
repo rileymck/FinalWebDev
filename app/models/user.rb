ab924584714bf3610d7bb51e306ea580e37ccb9c
Riley McKenzie <rmckenz4@msudenver.edu>
2024-11-16 09:14:35 -0700

    logout works, working on forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  #validataions
+  #validates that email is present, unique, and in the correct format
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
+  #validates that password is at least 6 characters long
+  validates :password, length: { minimum: 6 }, if: Proc.new { new_record? || !password.nil? }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'users#new'
+  root "users#new"
 end
